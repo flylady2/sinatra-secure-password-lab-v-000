9dc59365f076ed694645214f86ec3daf9eb4d20a
Leslie Stevens <flylady@mail.utexas.edu>
2018-11-07 18:14:08 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,7 @@ class ApplicationController < Sinatra::Base
 
   post "/signup" do
     user = User.new(:username => params[:username], :password => params[:password])
-    #binding.pry
+    binding.pry
     if params[:password] != "" && params[:username] != ""
       redirect "/login"
     else
